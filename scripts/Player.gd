9d6f6ce7d6d9ba3f25a9868582718d47e1331fef
Yann Burrett <yann.let@gmail.com>
2018-04-27 12:09:44 -0400

    added prototype tileset (1 tile) with collision applied gravity put animation logic in physics_process function

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,21 +1,31 @@
 extends KinematicBody2D
 
-export var speed = 250
-export var gravity = 50
-export var jump_height = 70
+export var speed = 500
+export var gravity = 25
+export var jump_height = -700
 var motion = Vector2()
+var up = Vector2(0,-1)
 export var friction = 0.25
 onready var animation = $Animation
+var current_animation = "idle"
 
 func _ready():
-	$Animation.animation = "idle"
+	pass
 
 func _physics_process(delta):
-	movement()
+	fall()
+	run()
+	jump()
+	move_and_slide(motion,up)
+	animation.animation = current_animation
 
-func movement():
-	var current_animation
-	
+func fall():
+	if is_on_floor() == false:
+		motion.y += gravity
+	else:
+		motion.y = 0
+
+func run():
 	if Input.is_action_pressed("ui_right"):
 		motion.x = speed
 		current_animation = "walk"
@@ -27,5 +37,9 @@ func movement():
 	else:
 		motion.x = lerp (motion.x, 0, friction)
 		current_animation = "idle"
-	animation.animation = current_animation
-	move_and_slide(motion)
+
+func jump():
+	if is_on_floor() == true:
+		if Input.is_action_pressed("ui_up"):
+			motion.y = jump_height
+			current_animation = "jump"
