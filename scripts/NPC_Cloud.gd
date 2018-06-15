c9df1c10444b2875d26b39fb2cdd980d7fd63e05
Yann Burrett <yann.let@gmail.com>
2018-06-15 17:06:32 +0100

    streamlined player detection for cloud npc (corrected collision mask)

--- scripts/NPC_Cloud.gd
+++ scripts/NPC_Cloud.gd
@@ -3,22 +3,23 @@ extends Node2D
 var timeout = false
 var lightning = preload("res://NPCs/Lightning.tscn")
 
+
 func _ready():
 	$Sprite/AnimationPlayer.current_animation = "float"
+
 
 func _physics_process(delta):
 	var body = $Sprite/RayCast2D.get_collider()
 	if $Sprite/RayCast2D.is_colliding():
-		if body.name == "Player":
-			fire()
-		else:
-			return
+		fire()
+
 
 func fire():
-	if timeout == false:
+	if not timeout:
 		$Sprite/RayCast2D.add_child(lightning.instance(), true)
 		$Timer.start()
 		timeout = true
+
 
 func _on_Timer_timeout():
 	timeout = false
