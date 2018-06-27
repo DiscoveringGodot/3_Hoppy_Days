09f4210377d761e43513b192a32ad725c6d713f2
Yann Burrett <yann.let@gmail.com>
2018-06-27 18:17:28 +0100

    fixed signal connection (added emit_signal to coin.gd)

--- scripts/Coin.gd
+++ scripts/Coin.gd
@@ -6,7 +6,7 @@ var taken = false # ensure the coin doesn't increment while despawning
 
 func _ready():
 	$AnimationPlayer.play("idle")
-	connect("body_entered", get_parent().get_parent().get_node("Player"), "OnCoinPickup")
+	connect("body_entered", get_node("/root/Level1/Player"), "OnCoinPickup")
 	# TODO remove get_parent() get_parent()
 
 func _physics_process(delta):
@@ -15,6 +15,7 @@ func _physics_process(delta):
 		if not taken:
 			taken = true
 			$AnimationPlayer.play("die")
+			emit_signal("body_entered")
 
 func die():  # called by AnimationPlayer node
 	queue_free()
